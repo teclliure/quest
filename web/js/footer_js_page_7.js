2090dabf96fbe1fecc79ab0e0d100012d134f977
Marc Montañés <marc@teclliure.net>
2013-02-27 08:07:19 +0100

    * Some rte bugs corrected ...

--- web/js/footer_js_page_7.js
+++ web/js/footer_js_page_7.js
@@ -11,15 +11,16 @@ $(function() {
     });
 
     $('textarea.wysiwyg').css('height','300px').css('width', '350px');
-    $('textarea.wysiwyg').wysihtml5({
-        "font-styles": true, //Font styling, e.g. h1, h2, etc. Default true
-        "emphasis": true, //Italics, bold, etc. Default true
-        "lists": true, //(Un)ordered lists, e.g. Bullets, Numbers. Default true
-        "html": true, //Button which allows you to edit the generated HTML. Default false
-        "link": false, //Button to insert a link. Default true
-        "image": false, //Button to insert an image. Default true,
-        "color": false //Button to change color of font
+    $('textarea.wysiwyg').each(function(){
+        $(this).wysihtml5({
+            "font-styles": true, //Font styling, e.g. h1, h2, etc. Default true
+            "emphasis": true, //Italics, bold, etc. Default true
+            "lists": true, //(Un)ordered lists, e.g. Bullets, Numbers. Default true
+            "html": true, //Button which allows you to edit the generated HTML. Default false
+            "link": false, //Button to insert a link. Default true
+            "image": false, //Button to insert an image. Default true,
+            "color": false //Button to change color of font
+        });
     });
-
 
 });
